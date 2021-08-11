dec4f5b9df33d46f9d45a57e2e327c3ab2076f5e
Juliano <julianosoder1989@gmail.com>
2021-08-11 09:26:45 -0300

    feat the fisrt attacks are in

--- src/components/attcks.js
+++ src/components/attcks.js
@@ -1,17 +1,45 @@
 import React, {Component, useState} from 'react'
 import "../App.css"
+import useSound from 'use-sound';
+import sayAttackPikachu from '../media/pikachu-attack.mp3'
+import sayThunderAttackPikachu from '../media/pikachuThunderAttack.mp3'
 
-const Attacks = () =>{
+const Attacks = ({triggerParentUpdate}) =>{
 
-    const main =()=>{
+    const [pikachuAttacksay] = useSound(sayAttackPikachu)
+    const [pikachuThundersay] = useSound(sayThunderAttackPikachu)
 
+    const attack = () =>{
+        if(attacking==='thundershock'){
+            triggerParentUpdate('thundershock')  
+            setTimeout(()=>{setattack('none')
+            document.getElementsByClassName('pokemon2')[0].style.display = 'flex'
+            },3200)
+        }else if(attacking==='agility'){
+            triggerParentUpdate('agility')  
+            setTimeout(()=>{setattack('none')
+            document.getElementsByClassName('pokemon2')[0].style.display = 'flex'
+            },1000)
+        }else if(attacking==='tackle'){
+            triggerParentUpdate('tackle')  
+            setTimeout(()=>{setattack('none')
+            document.getElementsByClassName('pokemon2')[0].style.display = 'flex'
+            },2000)
+        }
+        else{
+            triggerParentUpdate('none') 
+        }
+    }
+    const [attacking, setattack] = useState('none')
+    const main =()=>{   
         return(
             <div>
                 <dv className="attack-container">
-                    <button>ThunderShock</button>
-                    <button>Agility</button>
-                    <button>Tackle</button>
+                    <button onMouseDown={pikachuThundersay} onClick={()=> setattack('thundershock')}>ThunderShock</button>
+                    <button onMouseOver={pikachuAttacksay} onClick={()=> setattack('agility')}>Agility</button>
+                    <button onMouseDown={pikachuAttacksay} onClick={()=> setattack('tackle')}>Tackle</button>
                 </dv>
+                {attack()}
             </div>
         )
     }
--- src/components/battlefield.js
+++ src/components/battlefield.js
@@ -4,13 +4,35 @@ import pikachu2 from '../img/pikachuback2.png'
 import { Attacks } from './attcks'
 import "../App.css"
 import useSound from 'use-sound';
-import boopSfx from '../media/pikachu.mp3';
+import sayPikachu from '../media/pikachu.mp3';
+import thundershock from '../media/pikachuThundershock.gif'
+import agility from '../media/pikachuAgility.gif'
+import pikachuTacle from '../media/pikachuTacle.gif'
 
 const Battlefild = () => {
 
-    const [play] = useSound(boopSfx);
+    const [pikachuSay] = useSound(sayPikachu);
+    
     const [shift, setShift] = useState(0)
+    const [attack, setAttack] = useState('none')
 
+    const action = ()=>{
+        if(attack==='thundershock'){
+            document.getElementsByClassName('pokemon2')[0].style.display = 'none'
+            return <img className='attackgif' src={thundershock} alt='thundeshock!!' />
+        }else if(attack==='agility'){
+            document.getElementsByClassName('pokemon2')[0].style.display = 'none'
+            return <img className='attackgif' src={agility} alt="agility!!" />
+        }else if(attack==='tackle'){
+            document.getElementsByClassName('pokemon2')[0].style.display = 'none'
+            return <img className='attackgif' src={pikachuTacle} alt="tacke!!" />
+        }else{
+            return <span>wainting input</span>
+        }
+    }
+    const updatePageState = (state) => {
+        setAttack(state);
+    } 
     const main = () => {
         return(
             <div>
@@ -21,13 +43,15 @@ const Battlefild = () => {
                     </div>
                     <img src={pikachu} alt="pokemon" />
                 </div>
-                <div className="center"></div>
-                <div onClick={play} className="pokemon2">
-                    <img src={pikachu2} alt="" />
+                <div className="center">
+                    {action()}
+                </div>
+                <div className="pokemon2">
+                    <img onClick={pikachuSay} src={pikachu2} alt="" />
                     <div className="talk">
                         <p>My turn!</p>
                     </div>
-                    <Attacks />
+                    <Attacks triggerParentUpdate = {updatePageState}/>
                 </div>
                 </div>
             </div>
